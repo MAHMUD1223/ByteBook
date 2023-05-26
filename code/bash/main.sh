b47e78e6275150d2ba98853212c7c89ff1c233a7
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-05-26 02:50:12 +0000

    learnig bash untill 6ep

--- code/bash/main.sh
+++ code/bash/main.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
+: '
+ Important notes
+
+ `man test`
+ `echo $?`
+
+'
+
+: '
 name=$1
 echo "Hello $name "
+
+#      Maths
+expr 40 + 20
+expr 40 - 20
+expr 40 / 20
+expr 40 \* 20
+echo "$((10 + 20))"
+
+
+#    If Else
+mynum=209
+if [ $mynum -lt 200 ] # `-eq` for equal to. `-ne` for not equal of `!` in front of statement. `-gt` is for greater than. `-lt` for less than
+then
+   echo "True"
+else
+   echo "Flase condition"
+fi '
+if [ -d /workspace/haval/ ] # `-d` for dir check. `-f` for file check. to run cmd without [] run `command -v {cmd}`
+then
+   echo "exits"
+else
+   echo "does not exits"
+   fi
