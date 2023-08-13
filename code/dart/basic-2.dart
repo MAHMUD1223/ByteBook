95c2d7ead14b1587993812870141425a458b6599
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-08-13 06:22:41 +0600

    Create basic-2.dart

---
+++ code/dart/basic-2.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+}
+// functions
+void findPerimeter(int length, int breadth) {
+
+	int perimeter = 2 * (length + breadth);
+	print("The perimeter is $perimeter");
+}
+
+int getArea(int length, int breadth) {
+
+	int area = length * breadth;
+	return area;
+}
